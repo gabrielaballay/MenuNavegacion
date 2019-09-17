46ed06dd733ab3abc6a58f4ae616966847f716f1
gabriel <aballaygabriel@gmail.com>
2019-09-16 22:39:33 -0300

    Primer Commit

--- app/src/main/java/com/example/menunavegacion/ui/send/SendFragment.java
+++ app/src/main/java/com/example/menunavegacion/ui/send/SendFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,23 +12,23 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import com.example.menunavegacion.R;
 
 public class SendFragment extends Fragment {
 
-    private SendViewModel sendViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        sendViewModel =
-                ViewModelProviders.of(this).get(SendViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_send, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
-        sendViewModel.getText().observe(this, new Observer<String>() {
+        Button enviar=root.findViewById(R.id.enviar);
+        enviar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                Navigation.findNavController(getActivity(),R.id.nav_host_fragment).navigate(R.id.enviado);
             }
         });
         return root;
